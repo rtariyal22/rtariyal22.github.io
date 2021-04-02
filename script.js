ad1f28320c21bba5bdb25a24ad4d6a5d32b9ab10
rtariyal22 <rishabhtariyal22@gmail.com>
2021-04-02 17:51:50 +0100

    new changes

--- script.js
+++ script.js
@@ -1,7 +1,69 @@
-$('#workDiv').click(function(e){
-    $('.bg').css('background-image', 'url(images/Work.png)')
-    $('.customNavBar').css('background-color', 'rgb(23 24 23)')
-    $('.nav-link').attr('style','color:white !important');
-    $('#'+e.target.id).attr('style','color:#da8072 !important');
-    $('.galleryView').css('display', 'flex')
+
+$('#details').click(function(e){
+    url = window.location.href.split('/')
+    url = url.slice(0,url.length-1)
+    url.push('details.html')
+    window.location.href = url.join('/')
 })
+
+$('#details2').click(function(e){
+    url = window.location.href.split('/')
+    url = url.slice(0,url.length-1)
+    url.push('details2.html')
+    window.location.href = url.join('/')
+})
+
+$('#details3').click(function(e){
+    url = window.location.href.split('/')
+    url = url.slice(0,url.length-1)
+    url.push('details3.html')
+    window.location.href = url.join('/')
+})
+
+$('#details4').click(function(e){
+    url = window.location.href.split('/')
+    url = url.slice(0,url.length-1)
+    url.push('details4.html')
+    window.location.href = url.join('/')
+})
+$('#details5').click(function(e){
+    url = window.location.href.split('/')
+    url = url.slice(0,url.length-1)
+    url.push('details5.html')
+    window.location.href = url.join('/')
+})
+
+function onRightClick(pageId, max){
+    let map = window.ART_MAP
+    if (map === undefined){
+        map = {}
+    }
+    if (map[pageId] === undefined){
+        map[pageId] = 2
+    }else{
+        map[pageId] += 1
+    }
+    if (map[pageId] > max){
+        map[pageId] = 1
+    }
+    window.ART_MAP = map
+    $('.detailsImg').attr('src', 'images/artwork-'+pageId+'-'+ map[pageId] +'.png')
+}
+
+function onLeftClick(pageId, max){
+    let map = window.ART_MAP
+    if (map === undefined){
+        map = {}
+    }
+    if (map[pageId] === undefined){
+        map[pageId] = 1
+    }else{
+        map[pageId] -= 1
+        if (map[pageId] < 1){
+            map[pageId] = max
+        }
+
+    }
+    window.ART_MAP = map
+    $('.detailsImg').attr('src', 'images/artwork-'+pageId+'-'+ map[pageId] +'.png')
+}
